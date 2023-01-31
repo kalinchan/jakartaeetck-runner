79e3af3d3ef23dec244d974cd056ce2b248a80a7
Kalin Chan <kalin.chan@payara.fish>
2023-01-31 10:03:48 +0000

    Install Payara to bundles

--- .jenkins/modules/Setup/TestSetup.groovy
+++ .jenkins/modules/Setup/TestSetup.groovy
@@ -1,12 +1,6 @@
 echo "Copying Server Artifact"
 
 copyArtifacts(projectName: "${env.JOB_NAME}", selector: specific("${env.BUILD_NUMBER}"), flatten: true);
-def distribution = "${CFG.params.profile}"  == 'web' ? 'payara-web' : 'payara'
+def distribution = "${CFG.params.profile}"  == 'web' ? 'payara-web.zip' : 'payara.zip'
 echo "Installing Payara ${distribution}"
-sh "mvn install:install-file -DgroupId=fish.payara.distributions -DartifactId=${distribution} -Dversion=${CFG.'build.version'} -Dpackaging=zip -Dfile=${pwd()}/${distribution}.zip"
-
-echo "Unzipping Payara For Remote Use"
-sh "unzip -q ${pwd()}/${distribution}.zip"
-
-echo "Cleaning Up"
-sh "rm -rf ./${distribution}.zip"
+sh "mv ${distribution} ./bundles"
