64a690f8b793d96e545890b88cddc75cd75b6627
Kalin Chan <kalin.chan@payara.fish>
2023-02-01 13:47:08 +0000

    fixes

--- .jenkins/modules/Setup/TestSetup.groovy
+++ .jenkins/modules/Setup/TestSetup.groovy
@@ -3,5 +3,4 @@ echo "Copying Server Artifact"
 copyArtifacts(projectName: "${env.JOB_NAME}", selector: specific("${env.BUILD_NUMBER}"), flatten: true);
 def distribution = "${CFG.params.profile}"  == 'web' ? 'payara-web.zip' : 'payara.zip'
 echo "Installing Payara ${distribution}"
-sh "ls"
 sh "mv ${distribution} ./bundles"
